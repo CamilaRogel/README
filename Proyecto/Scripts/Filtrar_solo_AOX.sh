f063bd5054610d3b27e900d107081dbddc4e1d57
Camila Rogel <camirogel2002@gmail.com>
2025-07-16 22:15:10 -0500

    Elimina

--- Proyecto/Scripts/Filtrar_solo_AOX.sh
+++
@@ -1,3 +0,0 @@
-cd Fasta/
-awk '/^>/ {if (seq && tolower(header) ~ /aox/) print header "\n" seq; seq=""; header=$0; next} {seq=seq $0} END {if (seq && tolower(header) ~ /aox/) print header "\n" seq}' AOX_all.fasta > AOX_sequences_filtered.fasta
-
